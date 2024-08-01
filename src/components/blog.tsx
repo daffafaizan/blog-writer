9fa1b241b39bfd304bba80899b5f0c4a185847ab
Daffa <daffamfzn@gmail.com>
2024-08-02 01:13:01 +0700

    feat: added all required inputs to create a blog post

--- src/components/blog.tsx
+++ src/components/blog.tsx
@@ -10,39 +10,59 @@ import rehypeRaw from "rehype-raw";
 export default function Blog() {
   const [content, setContent] = useState("");
   return (
-    <div className="w-full h-full flex flex-row gap-4">
+    <div className="w-full h-full flex flex-col gap-2">
+      <div className="w-full flex flex-row gap-2">
+        <input
+          placeholder="Title"
+          className="w-1/2 border-2 border-gray-100 rounded-sm p-2"
+        />
+        <input
+          placeholder="Author"
+          className="w-1/2 border-2 border-gray-100 rounded-sm p-2"
+        />
+      </div>
       <textarea
-        className="w-1/2 h-full border-2 border-gray-100 rounded-sm mt-2 p-2"
-        placeholder="Create blog"
-        value={content}
-        name={content}
-        onChange={(e) => setContent(e.currentTarget.value)}
+        placeholder="Summary"
+        className="h-32 border-2 border-gray-100 rounded-sm p-2"
       />
-      <Markdown
-        className="w-1/2 h-full border-2 border-gray-100 rounded-sm text-justify text-sm mt-2 p-2"
-        remarkPlugins={[remarkGfm]}
-        rehypePlugins={[rehypeRaw]}
-        components={{
-          code(props) {
-            const { children, className, node, ...rest } = props as any;
-            const match = /language-(\w+)/.exec(className || "");
-            return match ? (
-              <SyntaxHighlighter
-                {...rest}
-                PreTag="div"
-                language={match[1]}
-                style={dark}
-              >
-                {String(children).replace(/\n$/, "")}
-              </SyntaxHighlighter>
-            ) : (
-              <code {...rest}>{children}</code>
-            );
-          },
-        }}
-      >
-        {content}
-      </Markdown>
+      <input
+        placeholder="Tags"
+        className="border-2 border-gray-100 rounded-sm p-2"
+      />
+      <div className="w-full h-full flex flex-row gap-2">
+        <textarea
+          className="w-1/2 h-full border-2 border-gray-100 rounded-sm p-2"
+          placeholder="Create blog"
+          value={content}
+          name={content}
+          onChange={(e) => setContent(e.currentTarget.value)}
+        />
+        <Markdown
+          className="w-1/2 h-full border-2 border-gray-100 rounded-sm text-justify text-sm p-2"
+          remarkPlugins={[remarkGfm]}
+          rehypePlugins={[rehypeRaw]}
+          components={{
+            code(props) {
+              const { children, className, node, ...rest } = props as any;
+              const match = /language-(\w+)/.exec(className || "");
+              return match ? (
+                <SyntaxHighlighter
+                  {...rest}
+                  PreTag="div"
+                  language={match[1]}
+                  style={dark}
+                >
+                  {String(children).replace(/\n$/, "")}
+                </SyntaxHighlighter>
+              ) : (
+                <code {...rest}>{children}</code>
+              );
+            },
+          }}
+        >
+          {content}
+        </Markdown>
+      </div>
     </div>
   );
 }
