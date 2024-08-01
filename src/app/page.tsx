1fc4aa8ecada07f02afe33b19c8ab3dde97483bc
Daffa <daffamfzn@gmail.com>
2024-08-02 00:55:10 +0700

    feat: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Markdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
+
+export default function Home() {
+  return (
+    <main>
+      <span>Test</span>
+    </main>
+  );
+}
