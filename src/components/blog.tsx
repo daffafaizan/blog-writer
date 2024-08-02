9c0fe177ca11aadc06c66b4cbceb4747eeedef79
Daffa <daffamfzn@gmail.com>
2024-08-02 09:56:56 +0700

    feat: added functionality to post blog post

--- src/components/blog.tsx
+++ src/components/blog.tsx
@@ -6,28 +6,73 @@ import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
+import axios from "axios";
 
 export default function Blog() {
+  const url = `${process.env.NEXT_PUBLIC_API_URL}/posts`;
+  const [title, setTitle] = useState("");
+  const [postAuthor, setPostAuthor] = useState("");
+  const [summary, setSummary] = useState("");
+  const [tags, setTags] = useState([]);
   const [content, setContent] = useState("");
+  const handleTags = (e: any) => {
+    let tags = e.currentTarget.value;
+    setTags(tags.split(" "));
+  };
+  const emptyForm = () => {
+    setTitle("");
+    setPostAuthor("");
+    setSummary("");
+    setTags([]);
+    setContent("");
+  };
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    const blogData = {
+      title,
+      postAuthor,
+      summary,
+      tags,
+      content,
+    };
+    const res = await axios.post(url, blogData);
+    console.log(res);
+    console.log(res.status);
+    if (res.status == 200) {
+      emptyForm();
+    }
+  };
   return (
-    <div className="w-full h-full flex flex-col gap-2">
+    <form onSubmit={handleSubmit} className="w-full h-full flex flex-col gap-2">
       <div className="w-full flex flex-row gap-2">
         <input
           placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.currentTarget.value)}
           className="w-1/2 border-2 border-gray-100 rounded-sm p-2"
+          required
         />
         <input
           placeholder="Author"
+          value={postAuthor}
+          onChange={(e) => setPostAuthor(e.currentTarget.value)}
           className="w-1/2 border-2 border-gray-100 rounded-sm p-2"
+          required
         />
       </div>
       <textarea
         placeholder="Summary"
+        value={summary}
+        onChange={(e) => setSummary(e.currentTarget.value)}
         className="h-32 border-2 border-gray-100 rounded-sm p-2"
+        required
       />
       <input
         placeholder="Tags"
+        value={tags}
+        onChange={(e) => handleTags(e)}
         className="border-2 border-gray-100 rounded-sm p-2"
+        required
       />
       <div className="w-full h-full flex flex-row gap-2">
         <textarea
@@ -63,6 +108,12 @@ export default function Blog() {
           {content}
         </Markdown>
       </div>
-    </div>
+      <button
+        type="submit"
+        className="w-full text-gray-700 bg-gray-100 hover:bg-gray-200 rounded-sm mt-1 py-2"
+      >
+        Send
+      </button>
+    </form>
   );
 }
